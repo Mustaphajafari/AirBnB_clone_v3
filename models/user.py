28af1a9534b8354be88f6a86d06223bdfb34c353
Mstaphajafari <jaafarimustapha28@gmail.com>
2024-02-05 13:23:02 -0800

    done

--- models/user.py
+++ models/user.py
@@ -1,51 +1,40 @@
 #!/usr/bin/python3
-"""
-User Class from Models Module
-"""
-import os
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-from hashlib import md5
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+import hashlib
 
 
 class User(BaseModel, Base):
-    """User class handles all application users"""
-    if storage_type == "db":
+    """Representation of a user """
+    if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column("password", String(128), nullable=False)
+        password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-
-        places = relationship('Place', backref='user', cascade='delete')
-        reviews = relationship('Review', backref='user', cascade='delete')
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """
-        initialize User Model, inherits from BaseModel
+            instantiates user object
         """
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                # Hash the password using MD5
+                secure = hashlib.md5()
+                secure.update(pwd.encode("utf-8"))
+                secure_password = secure.hexdigest()
+                kwargs['password'] = secure_password
         super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        """
-        getter for password
-        :return: password (hashed)
-        """
-        return self.__dict__.get("password")
-
-    @password.setter
-    def password(self, password):
-        """
-        Password setter, with md5 hasing
-        :param password: password
-        :return: nothing
-        """
-        self.__dict__["password"] = md5(password.encode('utf-8')).hexdigest()
